212e73fcffc930f494dd297fd9e4dd6009610d00
David Aerne <meodai@gmail.com>
2016-01-28 14:16:08 +0100

    creates better example for font use

--- demo/src/main.js
+++ demo/src/main.js
@@ -2,8 +2,12 @@ var Vue = require('vue');
 /* var css = require('!style!css!sass!../../main.scss'); */
 var base = require('./base.vue');
 var typography = require('./typography.vue');
-//var sassVariables = require("!!sass-variables!../../_0.settings.typography.scss");
-//console.log(sassVariables)
+
+/*
+var sassSettings = require("!!raw!sass!../../_0.settings.scss");
+console.log(sassSettings)
+*/
+
 new Vue({
   el: 'body',
   components: {
--- demo/src/typography.vue
+++ demo/src/typography.vue
@@ -1,6 +1,6 @@
 <script>
 export default {
-  components: {},
+  props: ['title', 'subtitle', 'lead'],
   data() {
     return {
       title: 'Lorem ipsum dor sit amet',
@@ -12,9 +12,55 @@ export default {
 </script>
 
 <style lang="sass">
-
 @import "../../0.settings";
 @import "../../mixins";
+
+$types: (
+  desktop: (
+    default: (
+      font-size     : 1rem,
+      font-family   : $t-default-font-family,
+      font-weight   : $t-regular,
+      font-style    : normal,
+      line-height   : 1.5
+    ),
+    title-page: (
+      font-weight   : $t-bold,
+      font-size     : 4rem,
+      line-height   : 1.3,
+      margin        : 0
+    ),
+    title-sub: (
+      font-weight   : $t-bold,
+      font-size     : 2.25rem,
+      line-height   : 1.3,
+      margin        : 0
+    ),
+    title: (
+      font-weight   : $t-bold,
+      font-size     : 1.75rem,
+      line-height   : 1.2,
+      margin        : 0 0 1em
+    ),
+    lead: (
+      font-size     : 1.2rem
+    ),
+    paragraph: (
+      font-size     : 1rem
+    )
+  ),
+  mobile: (
+    default: (
+      font-size     : 1.15rem,
+      line-height   : 1.5
+    ),
+    title-page: (
+      font-size     : 3rem,
+      border-bottom : 1px solid black,
+      margin-bottom : 0.25em
+    )
+  )
+);
 
 body {
   @include type;
@@ -22,11 +68,28 @@ body {
 
 @include textClasses(".t-");
 
+.content {
+  h2 {
+    @include type(title);
+  }
+  h2 + p {
+    @include type(lead, (margin-top: 0));
+  }
+  p {
+    @include type(paragraph);
+  }
+}
+
 </style>
 
 <template lang="jade">
 div.typography
-  h1.t-title {{ title }}
-  h2.t-subtitle {{ subtitle }}
-  p.t-paragraph {{ lead }}
+  h1.t-title-page {{ title }}
+  h2.t-title-sub {{ subtitle }}
+  p.t-lead {{ lead }}
+  .content
+    h2 Lorem Ipsum dolor sit amet
+    p this is the first paragraph
+    p this is an other paragraph
+
 </template>
