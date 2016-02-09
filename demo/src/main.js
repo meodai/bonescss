4f77c985ef41c638de8eeb08327303100e2cef1a
David Aerne <meodai@gmail.com>
2016-02-09 16:50:46 +0100

    adds sizes demo

--- demo/src/main.js
+++ demo/src/main.js
@@ -2,6 +2,7 @@ var Vue = require('vue');
 var base = require('./base.vue');
 var module = require('./module.vue');
 var typography = require('./typography.vue');
+var size = require('./size.vue');
 var prism = require('./prism.js')
 
 var header = require('./_header.vue');
@@ -12,6 +13,7 @@ new Vue({
     base: base,
     pageheader: header,
     module: module,
-    typography: typography
+    typography: typography,
+    size: size
   },
 });
---
+++ demo/src/size.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+}
+</script>
+
+<style lang="sass!sass-resources">
+/*$sizes: (
+  huge                  : $s-base * 3,
+  large                 : $s-base * 1.5,
+  medium                : $s-base,
+  small                 : $s-base * .5
+)*/
+
+@each $key, $size in $sizes {
+  .size--#{$key} {
+    max-width: $size;
+    height: $size;
+  }
+}
+
+.size {
+  display: inline-block;
+  margin-right: size(small);
+  overflow: hidden;
+  text-overflow: ellipsis;
+  vertical-align: top;
+  transition: 200ms max-width;
+  &:hover {
+    overflow: visible;
+    max-width: size(huge, 2);
+  }
+}
+
+</style>
+
+<template lang="jade">
+.demo__block
+  .demo__block.size.size--small small
+  .demo__block.size.size--medium medium
+  .demo__block.size.size--lrage large
+  .demo__block.size.size--huge huge
+</template>
